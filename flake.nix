8c77ff39d1d4e672c3dd1d5eda06df5a7ee9c08f
Rubsad <109631974+Rubsad@users.noreply.github.com>
2023-11-30 16:28:44 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Sample Nix Config";
+
+  outputs = {
+    nixpkgs,
+    ...
+  } @ inputs: {
+    nixosConfigurations = {
+      uwuki = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        nixpkgs = nixpkgs;
+        specialArgs = inputs;
+        modules = [
+          ./hosts/uwuki
+        ];
+      };
+    };
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+
+  nixConfig = {
+    experimental-features = ["nix-command" "flakes"];
+    substituters = [
+      "https://cache.nixos.org"
+    ];
+  };
+}
---
+++ hosts/uwuki/default.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, ... }:
+{
+  imports = [./hardware-configuration.nix];
+
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    systemd-boot.enable = true;
+  };
+
+  network = {
+    hostname = "uwuki";
+    networkmanager.enable = true;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings = {
+    trusted-users = ["rubsad"];
+    experimental.features = ["nix-command" "flakes"];
+    auto-optimize-store = true;
+  };
+
+  time.timezone = "Europe/Helsinki";
+
+  i18.defaultLocale = "en_US.UTF-8";
+  i18.extraLocaleSettings = {
+    LC_TIME = "fi_FI.UTF-8";
+    LC_PAPER = "fi_FI.UTF-8";
+    LC_ADRESS = "fi_FI.UTF-8";
+    LC_NUMERIC = "fi_FI.UTF-8";
+    LC_MONETARY = "fi_FI.UTF-8";
+    LC_TELEPHONE = "fi_FI.UTF-8";
+    LC_MEASUREMENT = "fi_FI.UTF-8";
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      X11Forwarding = false;
+      PermitRootLogin = "no";
+      PasswordAuthentication = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    nano
+    curl
+    wget
+    git
+  ];
+
+  environment.variables.EDITOR = "nano";
+
+  users.group.rubsad = {};
+
+  users.users.rubsad = {
+    hashedPassword = "";
+    home = "/home/rubsad";
+    isNormalUser = true;
+    extraGroups = [
+      "rubsad"
+      "users"
+      "networkmanager"
+      "wheel"
+    ];
+  };
+
+  system.stateVersion = "23.11";
+};
